1d020efec7a9c001232baa666655612aa06a934d
Andressa Bertolini <andressa@cavedigital.com.br>
2020-12-10 15:54:03 -0300

    API call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css'
+
+const api = {
+  key: "4e26469eb587d0e30e0d80f1415e8b14",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+
+      setQuery('London');
+      fetch(`${api.base}weather?q=${query}&id=524901&appid=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);  
+          console.log(result);
+        })
+
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app sunny">
+      <div className="gradient-background">
+        {dateBuilder(new Date())}
+
+        <button onClick={search}>Mudar</button>
+        São Paulo City
+        15º
+        Sunny
+      </div>
     </div>
   );
 }
