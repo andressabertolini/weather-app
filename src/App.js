8a11e4ad359cb6a0730153aa01ce55c31935c6e4
Andressa Bertolini <andressabtl@gmail.com>
2020-12-13 00:17:25 -0300

    Mobile adjustments

--- src/App.js
+++ src/App.js
@@ -30,14 +30,14 @@ function App() {
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   
-    let day = days[d.getDay()];
+    let weekday = week[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
+    return `${weekday} ${date} ${month} ${year}`
   }
 
   return (
